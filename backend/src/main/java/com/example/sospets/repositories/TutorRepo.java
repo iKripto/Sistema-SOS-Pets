7453402675b80cf505c9141be56432d5f277f765
iKripto <yagonascimento0288@gmail.com>
2025-11-19 09:48:55 -0300

    Problemas com string.

--- backend/src/main/java/com/example/sospets/repositories/TutorRepo.java
+++ backend/src/main/java/com/example/sospets/repositories/TutorRepo.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TutorRepo extends JpaRepository<Tutor,Long> {
+// CORREÇÃO AQUI: Mudamos de <Tutor, Long> para <Tutor, String>
+public interface TutorRepo extends JpaRepository<Tutor, String> {
+    
     Optional<Tutor> findByCpfOrNome(String cpf, String nome);
+    
     Optional<Tutor> findByCpf(String cpf);
+    
     void deleteByCpf(String cpf);
 }
