fbbbc1b70128f3eea92d7423b8570193c0afdd67
Nickolle <2411550981@fema.edu.br>
2025-12-10 10:22:07 -0300

    Corrigido erros de data de atendimento

--- backend/src/main/java/com/example/sospets/entities/Atendimento.java
+++ backend/src/main/java/com/example/sospets/entities/Atendimento.java
@@ -1,6 +1,7 @@
 package com.example.sospets.entities;
 
 import com.example.sospets.enums.CusteadoPor;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,40 +13,41 @@ import java.time.LocalDate; // Importante para as datas
 @NoArgsConstructor
 @Entity
 public class Atendimento {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    // --- Campos Novos (Vindos do Frontend) ---
     private String tipo; // castracao, tratamento, adocao
-    
-    @Column(columnDefinition = "TEXT") // Para textos longos
-    private String historico; 
-    
-    private String statusClinica; // aguardando, etc.
 
+    @Column(columnDefinition = "TEXT")
+    private String historico;
+
+    private String statusClinica;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dataGeracao;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dataEstimada;
 
-    // --- Campos Antigos (Mantenha se for usar, senão pode remover) ---
     private CusteadoPor custeadoPor;
     private String descricao;
     private double valor;
 
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "animal_id")
     private Animal animal;
 
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "clinica_id")
     private Clinica clinica;
 
-    @ManyToOne(cascade =  CascadeType.PERSIST, fetch = FetchType.EAGER)
-    @JoinColumn(name = "funcionario_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "voluntario_cpf", nullable = false)
     private Voluntario voluntario;
 
-    // O Frontend envia Tutor, precisamos mapear
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
-    @JoinColumn(name = "tutor_cpf") // Ou o nome da coluna correta do seu banco
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "tutor_cpf")
     private Tutor tutor;
 }
